4d135f105bad7ee01d0753a5f4a6dc4c3fc29cea
rtnapps <admin@realtnetworking.com>
2025-04-23 23:43:32 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Update package list
 apt-get update
-apt-get install -y libgl1
+
+# Install necessary dependencies
+apt-get install -y libgl1 libglib2.0-0
+
+# Start the application using gunicorn
 gunicorn -w 4 -b 0.0.0.0:8000 app:app
